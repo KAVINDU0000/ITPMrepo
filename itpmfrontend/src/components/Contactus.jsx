e4df3e489b2f6a1ae27552ad10b5c3e670bd32c0
IT22225542 <it22225542@my.sliit.lk>
2025-04-06 12:06:47 +0530

    add new links to the navbar

--- itpmfrontend/src/components/Contactus.jsx
+++ itpmfrontend/src/components/Contactus.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import './Contactus.css'
 import { FaPaw, FaMapMarkerAlt, FaPhone, FaEnvelope, FaClock } from "react-icons/fa";
 import { BsSend } from "react-icons/bs";
 
