1a0df93f91350884e12b7a245b721122804e1c13
IT22225542 <it22225542@my.sliit.lk>
2025-04-10 14:27:38 +0530

    Update Footer.jsx

--- itpmfrontend/src/components/Footer.jsx
+++ itpmfrontend/src/components/Footer.jsx
@@ -17,8 +17,8 @@ const Footer = () => {
           <div className="footer-links">
             <h4>Quick Links</h4>
             <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About Us</a></li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About Us</a></li>
               <li><a href="#services">Services</a></li>
               <li><a href="#blog">Blog</a></li>
               <li><a href="#contact">Contact</a></li>
